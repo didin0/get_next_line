b1027c5c5598feff5196b4e782e782999be482be
didin0 <mehdi.abbadi.pro@gmail.com>
2023-05-31 16:22:23 +0200

    bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabbadi <mabbadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 13:13:39 by mabbadi           #+#    #+#             */
-/*   Updated: 2023/05/31 16:16:54 by mabbadi          ###   ########.fr       */
+/*   Updated: 2023/05/26 13:52:17 by mabbadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,37 +61,36 @@ char	*get_next_line(int fd)
 {
 	char		*line;
 	char		*buf;
-	static char	*backup;
+	static char	*backup[OPEN_MAX];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	line = ft_read(fd, buf, backup);
+	line = ft_read(fd, buf, backup[fd]);
 	free(buf);
 	if (!line)
 	{
-		free(backup);
-		backup = NULL;
+		free(backup[fd]);
+		backup[fd] = NULL;
 		return (NULL);
 	}
-	backup = extract(line);
+	backup[fd] = extract(line);
 	return (line);
 }
 
 // int main()
 // {
 // 	int fd1 = open("read_error.txt", O_RDONLY);
-// 	// int fd2 = open("foo.txt", O_RDONLY);
+// 	int fd2 = open("foo.txt", O_RDONLY);
 // 	int fd;
 // 	int i = 0;
 
-// 	while (i++ < 1)
+// 	while (i++ < 6)
 // 	{
 // 		printf("fd1: %s", get_next_line(fd1));
-
-// 		// printf("fd2: %s", get_next_line(fd2));
+// 		printf("fd2: %s", get_next_line(fd2));
 // 	}
 // 	return 0;
 // }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabbadi <mabbadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 13:13:42 by mabbadi           #+#    #+#             */
-/*   Updated: 2023/05/31 16:19:33 by mabbadi          ###   ########.fr       */
+/*   Created: 2023/05/26 13:50:11 by mabbadi           #+#    #+#             */
+/*   Updated: 2023/05/31 16:19:16 by mabbadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -20,7 +20,7 @@
 # include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 10
 # endif
 # if BUFFER_SIZE > 10000
 #  undef BUFFER_SIZE
